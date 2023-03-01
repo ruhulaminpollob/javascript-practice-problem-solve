a73825d0443c6c77b8cf945ba8a1364edd278dda
ruhulaminpollob <ruhulamin.pollob@gmail.com>
2023-03-01 12:08:05 +0600

    practice problem 2 done

---
+++ practice-2.js
@@ -0,0 +1,16 @@
+/*
+Change the value stored in the storeFalsyValue variable to a falsy value,
+such that the code in the else statement executes.
+
+
+*/
+let storeFalsyValue = true;
+//
+storeFalsyValue=0;
+
+if(storeFalsyValue) {
+console.log(`Nothing to show!`);
+}
+else {
+console.log(`You are amazing!`);
+}
